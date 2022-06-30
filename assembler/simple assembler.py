30d4bc60a2dcb6099207f34f70dc9e9ba60b5c75
Divyansh <divyansh20060@iiitd.ac.in>
2022-06-30 23:00:59 +0530

    readfile

--- assembler/simple assembler.py
+++ assembler/simple assembler.py
@@ -1,3 +1,23 @@
 import sys
 
+def readfile():
+    counter = 0
+    for line in sys.stdin:
+        if line != "":
+            counter = counter+1
+            count = 0
+            for i in range(0,len(line)):
+                if line[i] == " ":
+                    count = count +1
+                else:
+                    count = 0
+                if count > 1 or line[i] == '\t':
+                    listerror["syntax error"] = counter
+                    break
+            listinp.append(line)
+
 print("ok")
+
+
+listinp = []
+listerror = []
