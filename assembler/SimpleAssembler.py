2a265ce9a087caaa5b9979b0dee4d0d198a6ff4d
Divyansh <divyansh20060@iiitd.ac.in>
2022-07-06 21:11:54 +0530

    quick commit

--- assembler/SimpleAssembler.py
+++ assembler/SimpleAssembler.py
@@ -12,7 +12,7 @@ def readfile():
                 else:
                     count = 0
                 if count > 1 or line[i] == '\t':
-                    listerror.append("invalid syntax error")
+                    listerror.append("invalid syntax error at line number " + str(counter))
                     break
 
             listinp.append(line)
@@ -40,11 +40,9 @@ def Label_Handling(input): #chckes for labels and halt
                         else:
                             listerror.append("hlt is not at the end")
                             return
-                # else:
-                #     listerror.append("invalid syntax error")
 
             else:
-                listerror.append("invalid syntax error")
+                listerror.append("invalid syntax error at line number " + str(j + 1))
 
         if split[0] == "hlt":
             halt = True
@@ -72,20 +70,20 @@ def Variable_Handling(input): #Checks for variables
                     var_dict[split[1]] = counter
                     counter = counter + 1
                 else:
-                    listerror.append("All variables must be defined at start")
+                    listerror.append("All variables must be defined at start. At line number " + str(j+1))
             else:
-                listerror.append("Invalid variable declaration")
+                listerror.append("Invalid variable declaration at line number " + str(j+1))
 
-def Register_Handling(reg): #Handles registers
+def Register_Handling(reg, j): #Handles registers
     if reg in reg_add and reg != "FLAGS":
         return reg_add[reg]
 
     else:
         if reg == "FLAGS":
-            listerror.append("Illegal use of FLAGS register")
+            listerror.append("Illegal use of FLAGS register at line number" + str(j + 1))
 
         else:
-            listerror.append("Invalid register value")
+            listerror.append("Invalid register value at line number" + str(j + 1))
 
 def d2b(num): #Converts decimal number to a binary number of 8 bits
 
@@ -110,7 +108,7 @@ def Assembler(input): #the main assembler function
             split = split[1:]
 
         if len(split) > 0 and split[0] not in Opp_Dict and split[0] != "var":
-            listerror.append("Invalid syntax error")
+            listerror.append("Invalid syntax error  at line number " + str(i + 1))
 
         if len(split) > 2 and split[0] == "mov" and split[2][0] == "$":
             Opp_Dict["mov"] = ("10010","B","2")
@@ -124,14 +122,14 @@ def Assembler(input): #the main assembler function
                 binary = binary + Opp_Dict[split[0]][0] + "00"      #opcode and unesd bits
 
                 for j in range(1,4):
-                    reg = Register_Handling(split[j])
+                    reg = Register_Handling(split[j],i)
                     if reg:
                         binary = binary + reg
 
             elif len(split) == 3 and Opp_Dict[split[0]][1] == "B":
                 binary = binary + Opp_Dict[split[0]][0]
 
-                reg = Register_Handling(split[1])
+                reg = Register_Handling(split[1],i)
                 if reg:
                     binary = binary + reg
 
@@ -144,7 +142,7 @@ def Assembler(input): #the main assembler function
 
             elif len(split) == 3 and Opp_Dict[split[0]][1] == "C":
                 binary = binary + Opp_Dict[split[0]][0] + "00000"
-                reg = Register_Handling(split[1])
+                reg = Register_Handling(split[1],i)
 
                 if reg:
                     binary = binary + reg
@@ -154,17 +152,17 @@ def Assembler(input): #the main assembler function
                     if ok in reg_add:
                         binary = binary + reg_add[ok]
                     else:
-                        listerror.append("Invalid register value")
+                        listerror.append("Invalid register value at line number " + str(i + 1))
 
                 else:
-                    reg = Register_Handling(split[2])
+                    reg = Register_Handling(split[2],i)
                     if reg:
                         binary = binary + reg
 
 
             elif len(split) == 3 and Opp_Dict[split[0]][1] == "D":
                 binary = binary + Opp_Dict[split[0]][0]
-                reg = Register_Handling(split[1])
+                reg = Register_Handling(split[1],i)
 
                 if reg:
                     binary = binary + reg
@@ -176,9 +174,9 @@ def Assembler(input): #the main assembler function
 
                 else:
                     if split[2] in label_dict:
-                        listerror.append("label given where variable is required")
+                        listerror.append("label given where variable is required at line number " + str(i + 1))
                     else:
-                        listerror.append("Undefined variable")
+                        listerror.append("Undefined variable at line number " + str(i + 1))
 
             elif len(split) == 2 and Opp_Dict[split[0]][1] == "E":
                 binary = binary + Opp_Dict[split[0]][0] + "000"
@@ -190,9 +188,9 @@ def Assembler(input): #the main assembler function
 
                 else:
                     if split[1] in var_dict:
-                        listerror.append("variable given where label is required")
+                        listerror.append("variable given where label is required at line number " + str(i + 1))
                     else:
-                        listerror.append("Undefined label")
+                        listerror.append("Undefined label at line number " + str(i + 1))
 
             elif len(split) == 1 and Opp_Dict[split[0]][1] == "F":
                 binary = binary + Opp_Dict[split[0]][0] + "00000000000"
@@ -242,13 +240,13 @@ label_dict ={}
 listout = []
 
 #input list with instructions
-listinp = ["var x","add r1 R2 R3","hlt"]
+listinp = []
 
 #list of errors generated
 listerror = []
 
 def main():
-    #readfile()
+    readfile()
     Label_Handling(listinp)
     Variable_Handling(listinp)
     Assembler(listinp)
